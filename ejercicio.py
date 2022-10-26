ea3b3b7c6e255009031869841582107a3389b994
ireneangulo <113629834+ireneangulo@users.noreply.github.com>
2022-10-26 17:16:04 +0200

    he creado unos ejercicios

---
+++ ejercicio.py
@@ -0,0 +1,20 @@
+class Alumno:
+    
+    def __init__(self,nombre,nota):
+        self.nombre = nombre
+        self.nota = nota
+        print("Alumno creado con exito")
+ 
+    def calificacion(self):
+        if self.nota < 5:
+            print("El alumno {} ha suspendido".format(self.nombre))
+        else:
+            print("El alumno {} ha aprobado".format(self.nombre))
+ 
+ # Creacion de alumnos
+alumno1 = Alumno("Juan", 7)
+alumno2 = Alumno("Alfredo", 3)
+ 
+# Comprobamos
+alumno1.calificacion()
+alumno2.calificacion()
